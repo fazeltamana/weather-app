065f686de81de834f04353ec77b06d396d72fbe5
fazeltamana <157019387+fazeltamana@users.noreply.github.com>
2024-11-02 16:21:32 +0600

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "f3e8359e69270d55det3ac4b4dc0of3a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchElement.value;
+  searchCity(searchElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
