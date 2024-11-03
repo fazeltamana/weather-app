b9b6bb982cd540174487175000a15f3c7f5a387a
fazeltamana <157019387+fazeltamana@users.noreply.github.com>
2024-11-03 12:28:38 +0600

    Make java script organized

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.condition.description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let windElement = document.querySelector("#wind-speed");
-  windElement.innerHTML = `${response.data.wind.speed}km/h`;
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  timeElement.innerHTML = formatDate(date);
   let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img
   src="${response.data.condition.icon_url}"
   class="weather-app-icon"/>`;
